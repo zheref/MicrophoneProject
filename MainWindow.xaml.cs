2afd120a2974d30cb368a9cf236642d539e7f7fb
Sergio Daniel Lozano Garcia <zheref@gmail.com>
2012-05-21 11:57:27 -0500

    Primera Version

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MicrophoneProject
+{
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+			
+			/*
+			SolidColorBrush brush = new SolidColorBrush();
+			brush.Color = Color.FromRgb(200, 4, 3);
+			Grid grid14 = new Grid();
+			grid14.Background = brush;
+			grid14.Width = 100;
+			grid14.Height = 200;
+			//grid1.Children.Add(grid14);
+			
+			ImageBrush ibrush = new ImageBrush();
+			ibrush.ImageSource = new BitmapImage(new Uri(@"C:\Users\USUARIO\Pictures\Wallpaper\gildart_fairy_tail_wallpaper_by_natsudrgonil-d33pq84.jpg", UriKind.Absolute));
+			
+			grid12.Background = ibrush;
+
+			// A partir de este punto se requiere la inserción de código para la creación del objeto.
+			*/
+		}
+	}
+}
---
+++ TimePicker.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicrophoneProject
+{
+	/// <summary>
+	/// Lógica de interacción para TimePicker.xaml
+	/// </summary>
+	public partial class TimePicker : UserControl
+	{
+		public TimePicker()
+		{
+			this.InitializeComponent();
+		}
+
+		private void Slider_ValueChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<double> e)
+		{
+			if(sldPicker.Value != 0 && sldPicker.Value != 5)
+			{
+				double d = Math.Truncate(sldPicker.Value * 10) / 10;
+				btnFree.Content = d + " mins";
+			}
+			else
+				btnFree.Content = "Libre";
+		}
+	}
+}
